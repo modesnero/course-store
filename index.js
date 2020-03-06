3bf531cf666f603b05d992cc9dc63f7b0873d32c
modesnero <modesnero@gmail.com>
2020-03-07 00:46:03 +0300

    check fs module

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+const fs = require('fs')
 const path = require('path')
 
-console.log(path.resolve(__dirname, './test', 'second.html'))
+const dirPath = path.join(__dirname, 'notes')
+const filePath = path.join(__dirname, 'notes', 'my-notes.txt')
+const newFilePath = path.join(__dirname, 'notes', 'notes.txt')
+
+fs.mkdir(dirPath, err => {
+  if (err) {
+    const isCreated = err.errno === -4075
+    if (isCreated) {
+      console.log('* Dir already created')
+    } else {
+      throw new Error(err)
+    }
+  } else {
+    console.log('* Dir was created')
+  }
+})
+
+fs.writeFile(filePath, 'Hello World', err => {
+  if (err) throw err
+  else console.log('* File was created')
+
+  fs.appendFile(filePath, '\nFrom append file', err => {
+    if (err) throw err
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) throw err
+      console.log(data)
+    })
+
+    fs.rename(filePath, newFilePath, err => {
+      if (err) throw err
+      console.log('* File was renamed :)')
+    })
+  })
+})
